383ddbeea268cfa39a8ac3eec3ebcfdffc044ae0
JRTitor <68708953+JRTitor@users.noreply.github.com>
2021-08-11 21:40:32 +0300

    Тесты для класса Person
    В первом курсе «Белый пояс по С++» у нас была задача «Имена и фамилии — 1». В ней надо было разработать класс Person, поддерживающий историю изменений человеком своих фамилии и имени. В данной задаче вам надо разработать юнит-тесты на реализацию класса Person. При разработке тестов учитывайте ограничения, которые накладывает на класс Person условие задачи «Имена и фамилии — 1».
    
    Начать работу вы можете с шаблона, который содержит наш фреймворк юнит-тестов и заготовку класса.
    
    test_person.cpp
    Условие задачи «Имена и фамилии — 1»
    Реализуйте класс для человека, поддерживающий историю изменений человеком своих фамилии и имени. 
    
    6789101112131415
    class Person {
    public:
      void ChangeFirstName(int year, const string& first_name) {
        // добавить факт изменения имени на first_name в год year
      }
      void ChangeLastName(int year, const string& last_name) {
        // добавить факт изменения фамилии на last_name в год year
      }
      string GetFullName(int year) {
        // получить имя и фамилию по состоянию на конец года year
    
    Считайте, что в каждый год может произойти не более одного изменения фамилии и не более одного изменения имени. При этом с течением времени могут открываться всё новые факты из прошлого человека, поэтому года́ в последовательных вызовах методов ChangeLastName и ChangeFirstName не обязаны возрастать.
    
    Гарантируется, что все имена и фамилии непусты.
    
    Строка, возвращаемая методом GetFullName, должна содержать разделённые одним пробелом имя и фамилию человека по состоянию на конец данного года.
    
    Если к данному году не случилось ни одного изменения фамилии и имени, верните строку "Incognito".
    Если к данному году случилось изменение фамилии, но не было ни одного изменения имени, верните "last_name with unknown first name".
    Если к данному году случилось изменение имени, но не было ни одного изменения фамилии, верните "first_name with unknown last name".
    Примечание
    Перед отправкой вашего файла убедитесь, что он не содержит класса Person. В противном случае вы получите ошибку компиляции. Наша тестирующая система умеет игнорировать функции в файлах (поэтому, например, в задаче "Тесты для функции IsPalindrom" необязательно удалять функцию IsPalindrom), но не умеет это делать для классов.

---
+++ yellowBelt/week2_3.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+template <class T>
+ostream& operator << (ostream& os, const vector<T>& s) {
+  os << "{";
+  bool first = true;
+  for (const auto& x : s) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << x;
+  }
+  return os << "}";
+}
+
+template <class T>
+ostream& operator << (ostream& os, const set<T>& s) {
+  os << "{";
+  bool first = true;
+  for (const auto& x : s) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << x;
+  }
+  return os << "}";
+}
+
+template <class K, class V>
+ostream& operator << (ostream& os, const map<K, V>& m) {
+  os << "{";
+  bool first = true;
+  for (const auto& kv : m) {
+    if (!first) {
+      os << ", ";
+    }
+    first = false;
+    os << kv.first << ": " << kv.second;
+  }
+  return os << "}";
+}
+
+template<class T, class U>
+void AssertEqual(const T& t, const U& u, const string& hint = {}) {
+  if (t != u) {
+    ostringstream os;
+    os << "Assertion failed: " << t << " != " << u;
+    if (!hint.empty()) {
+      os << " hint: " << hint;
+    }
+    throw runtime_error(os.str());
+  }
+}
+
+void Assert(bool b, const string& hint) {
+  AssertEqual(b, true, hint);
+}
+
+class TestRunner {
+public:
+  template <class TestFunc>
+  void RunTest(TestFunc func, const string& test_name) {
+    try {
+      func();
+      cerr << test_name << " OK" << endl;
+    } catch (exception& e) {
+      ++fail_count;
+      cerr << test_name << " fail: " << e.what() << endl;
+    } catch (...) {
+      ++fail_count;
+      cerr << "Unknown exception caught" << endl;
+    }
+  }
+
+  ~TestRunner() {
+    if (fail_count > 0) {
+      cerr << fail_count << " unit tests failed. Terminate" << endl;
+      exit(1);
+    }
+  }
+
+private:
+  int fail_count = 0;
+};
+/*
+class Person {
+public:
+  // Вы можете вставлять сюда различные реализации класса,
+  // чтобы проверить, что ваши тесты пропускают корректный код
+  // и ловят некорректный
+  void ChangeFirstName(int year, const string& first_name) {
+  }
+  void ChangeLastName(int year, const string& last_name) {
+  }
+  string GetFullName(int year) {
+  }
+};*/
+class Person {
+public:
+  void ChangeFirstName(const int& year, const string& first_name) {
+    logName[year] = first_name;
+  }
+  void ChangeLastName(const int& year, const string& last_name) {
+    logSec[year] = last_name;
+  }
+  string GetFullName(int year) {
+    string name = getName(year, logName);
+    string sec = getName(year, logSec);
+    string full = name + " " + sec;
+    if (name.size() == 0 && sec.size() == 0)  return "Incognito";
+    if (name.size() == 0) return sec + " with unknown first name";
+    if (sec.size() == 0) return name + " with unknown last name";
+    return full;
+}
+private:
+
+  string getName(int year, map<int, string>& names) {
+    string name;
+    for(const auto& item: names) {
+      if(item.first <= year) name = item.second;
+      else break;
+    }
+    return name;
+  }
+  map<int,string> logName;
+  map<int,string> logSec;
+
+};
+
+void TestVoidSpace() {
+  {
+    Person p;
+    p.ChangeLastName(1489, "Oda");
+    AssertEqual(p.GetFullName(1489), "Oda with unknown first name", "TestVoidSpace #1");
+    p.ChangeFirstName(1480, "Nabunaga");
+    AssertEqual(p.GetFullName(1480), "Nabunaga with unknown last name", "TestVoidSpace #2");
+    p.ChangeFirstName(1582, "卍");
+    AssertEqual(p.GetFullName(1582), "卍 Oda", "TestVoidSpace #3");
+    p.ChangeLastName(1583, "卍");
+    AssertEqual(p.GetFullName(1583), "卍 卍", "TestVoidSpace #4");
+  }
+  Person p;
+  AssertEqual(p.GetFullName(1), "Incognito", "TestVoidSpace #5");
+}
+
+
+int main() {
+  TestRunner runner;
+  runner.RunTest(TestVoidSpace, "TestVoidSpace");
+  // добавьте сюда свои тесты
+  return 0;
+}
